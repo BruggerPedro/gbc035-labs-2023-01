d83a7252c9961bb8d8749178d8c7606b37fbf5cb
Pedro Brügger <65832126+BruggerPedro@users.noreply.github.com>
2023-08-03 23:39:26 -0300

    Create HelloWorld.java

---
+++ LAB01 - Procedural/ex01/HelloWorld.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        System.out.println("Meu nome eh Pedro Murilo Brugger");
+        System.out.println("Minha matricula eh: 12011BCC054");
+    }
+}
